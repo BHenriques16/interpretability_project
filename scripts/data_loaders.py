d51f48fc9ffcb467f710ec31e5df49e2572b0ccf
BHenriques16 <145695449+BHenriques16@users.noreply.github.com>
2025-11-08 20:37:24 +0000

    data load and split

--- scripts/data_loaders.py
+++ scripts/data_loaders.py
@@ -2,19 +2,23 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 
+
 # Define device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-# Resize to 128x128, convert to tensor, and normalize
-transform = transforms.Compose([
+# Resize to 128x128, convert to tensor, and normalize and data augmentation for better generalization
+train_transform = transforms.Compose([
     transforms.Resize((128, 128)),
+    transforms.RandomHorizontalFlip(),  # random horizontal flip
+    transforms.RandomRotation(15),      # random rotation up to 15 degrees
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),  # random brightness and contrast adjustment
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    transforms.Normalize([0.5]*3, [0.5]*3)
 ])
 
 # Load complete CelebA dataset
-celeba_dataset = datasets.CelebA(root='./data', split='all', transform=transform, download=True)
+celeba_dataset = datasets.CelebA(root='./data', split='all', transform=train_transform, download=True)
 
 # Dataset split sizes train/val/test: 70%, 15%, 15%
 total_size = len(celeba_dataset)
