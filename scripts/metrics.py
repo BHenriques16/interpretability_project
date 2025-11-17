3adee973fb16b88579e14255e48770641d6e0516
BHenriques16 <bernardovhenriques@gmail.com>
2025-11-17 11:33:12 +0000

    metrics implementation

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -1,1 +1,28 @@
-print('Hello World')
+import numpy as np
+
+# Calulus of metrics for completeness
+def completeness(explanation, prediction, threshold=0.5):
+    # Normalizes the explanation
+    explanation = explanation / np.max(explanation)
+    # Binarizes the explanation with the threshold
+    explanation_binary = (explanation > threshold).astype(float)
+    # Calculates the sum of importances
+    completeness_score = np.sum(explanation_binary * prediction) / np.sum(prediction)
+    return completeness_score
+
+
+# Calulus of metrics for attribution localization
+def attribution_localization(explanation, ground_truth, threshold=0.5):
+    # Normalizes the explanation
+    explanation = explanation / np.max(explanation)
+    # Binarizes the explanation with the threshold
+    explanation_binary = (explanation > threshold).astype(float)
+    # Calculates the intersection between explanation and ground truth
+    intersection = np.sum(explanation_binary * ground_truth)
+    # Calculates the union between explanation and ground truth
+    union = np.sum(explanation_binary) + np.sum(ground_truth) - intersection
+    # Calculates the Attribution Localization metric
+    if union == 0:
+        return 0.0
+    attribution_localization_score = intersection / union
+    return attribution_localization_score
